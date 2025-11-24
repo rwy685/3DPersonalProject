20e3755396fd26bb4bc28ab8be3d5c24dfd125fd
rwy685 <rwy685@gmail.com>
2025-11-24 11:48:20 +0900

    Settings.Dotween,CSfiles

--- Assets/02_Scripts/Player.cs
+++ Assets/02_Scripts/Player.cs
@@ -1,18 +1,12 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private PlayerCondition condition;
+    private PlayerStatus status;
+    private PlayerController controller;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
--- Assets/03_Managers/GameManager.cs
+++ Assets/03_Managers/GameManager.cs
@@ -1,18 +1,57 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static GameManager instance;
+
+    public static GameManager Instance
     {
-        
+        get
+        {
+            if (instance == null)
+            {
+                GameObject go = new GameObject("GameManager");
+                instance = go.AddComponent<GameManager>();
+            }
+            return instance;
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public CharacterManager CharacterManager { get; private set; }
+    public UIManager UIManager { get; private set; }
+
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            InitializeManagers();
+
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
+
+    private void InitializeManagers()
+    {
+        if (CharacterManager == null)
+        {
+            var cmobj = new GameObject("CharacterManager");
+            CharacterManager = cmobj.AddComponent<CharacterManager>();
+        }
+
+        if (UIManager == null)
+        {
+            var uiobj = new GameObject("UIManager");
+            UIManager = uiobj.AddComponent<UIManager>();
+        }
+    }
+
+
 }
